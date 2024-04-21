b89b1788d2c929776437037e3ee526bdd112b8b9
root <root@localhost.localdomain>
2024-04-21 20:50:42 +0800

    events logs

--- cmd/test/monitor.go
+++ cmd/test/monitor.go
@@ -2,84 +2,101 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"time"
-
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 )
 
+// Event 结构体定义
 type Event struct {
-	ID        string    // 容器ID
-	Action    Action    // 动作
-	Timestamp time.Time // 时间戳
+	ID         string            `json:"id"`
+	Status     string            `json:"status"`
+	Time       time.Time         `json:"time"`
+	From       string            `json:"from"`
+	Action     string            `json:"action"`
+	Attributes map[string]string `json:"attributes"`
 }
 
-// Action represents a Docker event action
-type Action string
-
-//const (
-//	ActionStart = Action("start")
-//	ActionStop  = Action("stop")
-//	ActionKill  = Action("kill")
-//)
-
-var monitorCmd = &cobra.Command{
-	Use:   "monitor",
-	Short: "Start container monitoring",
+// eventsCmd represents the events command
+var eventsCmd = &cobra.Command{
+	Use:   "events",
+	Short: "Monitor Docker events and store logs",
+	Long:  `Monitor Docker events and store logs to specified directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		startMonitoring(cmd)
+		logDir, err := cmd.Flags().GetString("log-dir")
+		if err != nil {
+			log.Fatalf("Error getting log directory: %v", err)
+		}
+
+		if logDir == "" {
+			log.Fatalf("Please provide a log directory with the --log-dir flag.")
+		}
+
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			log.Fatalf("Error creating log directory: %v", err)
+		}
+
+		cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+		if err != nil {
+			log.Fatalf("Error creating Docker client: %v", err)
+		}
+
+		events, errChan := cli.Events(context.Background(), types.EventsOptions{})
+		if err != nil {
+			log.Fatalf("Error getting Docker events: %v", err)
+		}
+
+		for {
+			select {
+			case event, ok := <-events:
+				if !ok {
+					// 通道已关闭，没有更多的事件
+					return
+				}
+				eventJSON, err := json.Marshal(event)
+				if err != nil {
+					log.Printf("Error marshaling event: %v", err)
+					continue
+				}
+
+				eventID := event.ID
+				if eventID == "" {
+					eventID = "unknown"
+				}
+
+				logFilePath := filepath.Join(logDir, fmt.Sprintf("%s.log", eventID))
+				logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					log.Printf("Error opening log file: %v", err)
+					continue
+				}
+				defer logFile.Close()
+
+				if _, err := logFile.WriteString(string(eventJSON) + "\n"); err != nil {
+					log.Printf("Error writing to log file: %v", err)
+				}
+			case err := <-errChan:
+				// 从错误通道读取到错误
+				log.Fatalf("Error reading Docker events: %v", err)
+			}
+		}
 	},
 }
 
-func startMonitoring(cmd *cobra.Command) {
-	logFile, _ := cmd.Flags().GetString("log")
-
-	cli, err := client.NewClientWithOpts()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	events := make(map[string][]Event)
-
-	ctx := context.Background()
-
-	eventChan, errChan := cli.Events(ctx, types.EventsOptions{})
-
-	fmt.Println("容器监控程序已启动...")
-
-	logFilePtr, err := os.Create(logFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFilePtr.Close()
-
-	logger := log.New(logFilePtr, "", log.LstdFlags)
-
-	for {
-		select {
-		case event := <-eventChan:
-			e := Event{
-				ID:        event.Actor.ID,
-				Action:    Action(event.Action),
-				Timestamp: time.Now(),
-			}
-
-			events[event.Actor.ID] = append(events[event.Actor.ID], e)
-
-			logger.Printf("容器ID：%s，动作：%s，时间：%s\n", e.ID, e.Action, e.Timestamp.String())
-
-			fmt.Printf("接收到事件：%s\n", e.Action)
-
-		case err := <-errChan:
-			fmt.Printf("发生错误：%v\n", err)
-		}
-	}
-}
-
 func init() {
-	rootCmd.AddCommand(monitorCmd)
+	eventsCmd.Flags().StringP("log-dir", "l", "", "Path to the directory where logs will be stored")
+	rootCmd.AddCommand(eventsCmd)
 }
+
+//func main() {
+//	if err := rootCmd.Execute(); err != nil {
+//		fmt.Println(err)
+//		os.Exit(1)
+//	}
+//}
